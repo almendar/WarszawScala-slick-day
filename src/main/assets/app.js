813e4f838e282dd634aeee6f6dc19137d5d9eb52
Rafał Krzewski <rafal.krzewski@caltha.pl>
2015-03-24 23:52:32 +0100

    basic routing + necessary scaffolding

--- src/main/assets/app.js
+++ src/main/assets/app.js
@@ -1,11 +1,36 @@
-(function(){
+(function() {
 
-    var slick = angular.module("slick", ["ngMaterial"]);
+    var slick = angular.module("slick", ["ngRoute", "ngMaterial"]);
 
     slick.config(function($mdThemingProvider, $mdIconProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('indigo')
             .accentPalette('lime');
     });
+    
+	slick.config(function($routeProvider) {
+		$routeProvider.when("/books", {
+			templateUrl : "partial/books.html",
+			controllerAs : "BooksCtrl"
+		});
+		$routeProvider.when("/authors", {
+			templateUrl : "partial/authors.html",
+			controllerAs : "AutorsCtrl"
+		});
+		$routeProvider.when("/categories", {
+			templateUrl : "partial/categories.html",
+			controllerAs : "CategoriesCtrl"
+		});
+		$routeProvider.otherwise("/books");
+	});
+
+	slick.controller("BooksCtrl", function() {
+	});
+
+	slick.controller("AuthorsCtrl", function() {
+	});
+
+	slick.controller("CategoriesCtrl", function() {
+	});
 
 })();
