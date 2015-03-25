2e08eee074cba6140e35c87f2bee2b77a4d5c854
Rafał Krzewski <rafal.krzewski@caltha.pl>
2015-03-26 00:00:43 +0100

    Main list views, initial REST backend integration, mock backend for view testing.

--- src/main/assets/app.js
+++ src/main/assets/app.js
@@ -1,25 +1,42 @@
 (function() {
 
-    var slick = angular.module("slick", ["ngRoute", "ngMaterial"]);
+	var mockBackend = false;
 
-    slick.config(function($mdThemingProvider, $mdIconProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('indigo')
-            .accentPalette('lime');
-    });
-    
+	var slick = angular.module("slick", ["ngRoute", "ngResource", "ngMaterial"]);
+
+	slick.config(function($mdThemingProvider, $mdIconProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('indigo')
+			.accentPalette('lime');
+	});
+	
 	slick.config(function($routeProvider) {
 		$routeProvider.when("/books", {
 			templateUrl : "partial/books.html",
-			controllerAs : "BooksCtrl"
+			controller : "BooksCtrl as ctrl",
+			resolve : {
+				items : function(backend) {
+					return backend.books.query();
+				}
+			}
 		});
 		$routeProvider.when("/authors", {
 			templateUrl : "partial/authors.html",
-			controllerAs : "AutorsCtrl"
+			controller : "AuthorsCtrl as ctrl",
+			resolve : {
+				items : function(backend) {
+					return backend.authors.query();
+				}
+			}
 		});
 		$routeProvider.when("/categories", {
 			templateUrl : "partial/categories.html",
-			controllerAs : "CategoriesCtrl"
+			controller : "CategoriesCtrl as ctrl",
+			resolve : {
+				items : function(backend) {
+					return backend.categories.query();
+				}
+			}
 		});
 		$routeProvider.otherwise("/books");
 	});
@@ -56,13 +73,98 @@
 		});
 	});
 
-	slick.controller("BooksCtrl", function() {
+	slick.controller("BooksCtrl", function(items) {
+		this.items = items;
 	});
 
-	slick.controller("AuthorsCtrl", function() {
+	slick.controller("AuthorsCtrl", function(items) {
+		this.items = items;
 	});
 
-	slick.controller("CategoriesCtrl", function() {
+	slick.controller("CategoriesCtrl", function(items, backend) {
+		this.items = items;
+		
+		this.icon = function(item) {
+			if(item.hasChildren && !item.expanded) {
+				return "fi-pricetag-multiple";
+			} else {
+				return "fi-price-tag";
+			}
+		};
+
+		this.toggle = function(item) {
+			if(item.hasChildren) {
+				item.expanded = !item.expanded;
+				if(item.expanded && !item.children) {
+					backend.category.query(item.id).then(function(children) {
+						item.children = children;
+					})
+				}				
+			}
+		};
+	});
+
+	slick.service("backend", function($resource, $q) {
+		if(mockBackend) {
+			return {
+				books : {
+					query : function() {
+						return [{
+							"id": 0,
+							"authors": [{
+								"id": 0,
+								"name": "F. Scott Fitzgerald"
+							}],
+							"category": {
+								"id": 2,
+								"name": "Rich people",
+								"parentId": 1,
+								"hasChildren": false
+							},
+							"title": "The Great Gatsby",
+							"publishDate": -16142
+						}];
+					}
+				},
+				authors : {
+					query : function() {
+						return [{
+							"id": 0,
+							"name": "F. Scott Fitzgerald"
+						}];
+					}
+				},
+				categories : {
+					query : function() {
+						return [{
+							"id" : 1,
+							"name" : "Root category",
+							"hasChildren" : true
+						}];
+					}
+				},
+				category : {
+					query : function(id) {
+						return $q(function(resolve, reject) {
+							resolve([{
+								"name" : "Category with children",
+								"hasChildren" : true
+							}, {
+								"name" : "Category with no children",
+								"hasChildren" : false
+							}]);
+						});
+					}
+				}
+			}
+		} else {
+			return {
+				books : $resource("rest/books"),
+				authors : $resource("rest/authors"),
+				categories : $resource("rest/categories"),
+				category : $resource("rest/categories/:id"),
+			}			
+		}
 	});
 
 })();
